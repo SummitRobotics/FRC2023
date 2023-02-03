d3bf6532971c47baad57080294bd8666e67f1ff4
Owen McCormick <oweninoregon@gmail.com>
2023-02-03 14:08:02 -0800

    Finished refactoring homeable

--- src/main/java/frc/robot/utilities/homing/HomeableCANSparkMax.java
+++ src/main/java/frc/robot/utilities/homing/HomeableCANSparkMax.java
@@ -1,71 +1,174 @@
 package frc.robot.utilities.homing;
 
+import java.util.function.BooleanSupplier;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.utilities.RollingAverage;
 
 /**
- * A Homeable class for CANSparkMax
+ * A class containing home by current settings for a CANSparkMax
  */
-public class HomeableCANSparkMax implements Homeable {
+public class HomeableCANSparkMax {
 
+    public enum Type {
+        ByCurrent,
+        BySwitch,
+    }
+
+    private final RollingAverage currentAverage = new RollingAverage(10, false);
+    private Type type;
     private CANSparkMax motor;
     private Subsystem subsystem;
     private double homingPower;
+    private double currentThreshold;
+    private double forwardLimit;
+    private double reverseLimit;
+    private BooleanSupplier switchCondition;
+    private boolean softLimits;
+    private boolean isFinished;
 
-    public HomeableCANSparkMax(CANSparkMax motor, Subsystem subsystem, double homingPower) {
+    // by current, no soft limits
+    public HomeableCANSparkMax(
+        CANSparkMax motor,
+        Subsystem subsystem,
+        double homingPower,
+        double currentThreshold
+    ) {
+
+        type = Type.ByCurrent;
+        softLimits = false;
         this.motor = motor;
         this.subsystem = subsystem;
+        this.homingPower = homingPower;
+        this.currentThreshold = currentThreshold;
+        forwardLimit = 0;
+        reverseLimit = 0;
+        switchCondition = () -> false;
+        isFinished = false;
     }
 
-    @Override
-    public double getCurrent() {
-        return motor.getOutputCurrent();
+    // by switch, no soft limits
+    public HomeableCANSparkMax(
+        CANSparkMax motor,
+        Subsystem subsystem,
+        double homingPower,
+        BooleanSupplier switchCondition
+    ) {
+        type = Type.BySwitch;
+        softLimits = false;
+        this.motor = motor;
+        this.subsystem = subsystem;
+        this.homingPower = homingPower;
+        currentThreshold = 0;
+        forwardLimit = 0;
+        reverseLimit = 0;
+        this.switchCondition = switchCondition;
+        isFinished = false;
     }
 
-    @Override
-    public double getVelocity() {
-        return motor.getEncoder().getVelocity();
+    // by current, soft limits
+    public HomeableCANSparkMax(
+        CANSparkMax motor,
+        Subsystem subsystem,
+        double homingPower,
+        double currentThreshold,
+        double forwardLimit,
+        double reverseLimit
+    ) {
+        type = Type.ByCurrent;
+        softLimits = true;
+        this.motor = motor;
+        this.subsystem = subsystem;
+        this.homingPower = homingPower;
+        this.currentThreshold = currentThreshold;
+        this.forwardLimit = forwardLimit;
+        this.reverseLimit = reverseLimit;
+        switchCondition = () -> false;
+        isFinished = false;
     }
 
-    // Power defaults to provided homing power
+    // by switch, soft limits
+    public HomeableCANSparkMax(
+        CANSparkMax motor,
+        Subsystem subsystem,
+        double homingPower,
+        BooleanSupplier switchCondition,
+        double forwardLimit,
+        double reverseLimit
+    ) {
+        type = Type.BySwitch;
+        softLimits = true;
+        this.motor = motor;
+        this.subsystem = subsystem;
+        this.homingPower = homingPower;
+        currentThreshold = 0;
+        this.forwardLimit = forwardLimit;
+        this.reverseLimit = reverseLimit;
+        this.switchCondition = switchCondition;
+        isFinished = false;
+    }
 
-    @Override
+    public boolean getIsFinished() {
+        return isFinished;
+    }
+
+    public void setIsFinished() {
+        this.isFinished = true; 
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public boolean hasSoftLimits() {
+        return softLimits;
+    }
+
+    public void updateCurrent() {
+        currentAverage.update(motor.getOutputCurrent());
+    }
+
+    public void resetCurrent() {
+        currentAverage.reset();
+    }
+
     public void setPower(double power) {
         motor.set(power);
     }
 
-    @Override
     public void setPower() {
         motor.set(homingPower);
     }
 
-    @Override
-    public void setHome(double position) {
-        motor.getEncoder().setPosition(position);
+    public void zeroHome() {
+        motor.getEncoder().setPosition(0);
     }
 
-    @Override
-    public void setSoftLimits(double forward, double reverse) {
-        motor.setSoftLimit(SoftLimitDirection.kForward, (float) forward);
-        motor.setSoftLimit(SoftLimitDirection.kReverse, (float) reverse);
-    }
-
-    @Override
     public void disableSoftLimits() {
         motor.enableSoftLimit(SoftLimitDirection.kForward, false);
         motor.enableSoftLimit(SoftLimitDirection.kReverse, false);
     }
 
-    @Override
     public void enableSoftLimits() {
+        motor.setSoftLimit(SoftLimitDirection.kForward, (float) forwardLimit);
+        motor.setSoftLimit(SoftLimitDirection.kReverse, (float) reverseLimit);
         motor.enableSoftLimit(SoftLimitDirection.kForward, true);
         motor.enableSoftLimit(SoftLimitDirection.kReverse, true);
     }
 
-    @Override
+    public boolean stopCondition() {
+        switch (type) {
+            case ByCurrent:
+                return currentAverage.getAverage() >= currentThreshold;
+            case BySwitch:
+                return switchCondition.getAsBoolean();
+            default:
+                return false;
+        }
+    }
+
     public Subsystem getSubsystemObject() {
         return subsystem;
     }
-    
 }
--- src/main/java/frc/robot/utilities/homing/HomeableSubsystem.java
+++ src/main/java/frc/robot/utilities/homing/HomeableSubsystem.java
@@ -1,8 +1,12 @@
 package frc.robot.utilities.homing;
 
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
 /**
- * A functional interface that subsystems with homeable components will implement
+ * An interface that subsystems with homeable motors will implement
  */
 public interface HomeableSubsystem {
-    public Homeable[] getHomeables();
+    public HomeableCANSparkMax[] getHomeables();
+
+    public Subsystem getSubsystemObject();
 }
