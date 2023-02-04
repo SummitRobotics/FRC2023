3f2125b06499ef2b063c4219d166292b9bced38a
Eric Verheyden <erve2005@gmail.com>
2023-02-03 19:10:00 -0800

    maybe fixing git history

--- DriverstationDashboard/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('ntClient', {
-  set: (id, val) => ipcRenderer.invoke('assign', id, val),
-  isReady: () => ipcRenderer.invoke('ready'),
-})
