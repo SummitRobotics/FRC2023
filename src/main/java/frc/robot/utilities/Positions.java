2fdf730bfecdf0c8ed7698e59c5aa470b9d1d6c1
Eric Verheyden <erve2005@gmail.com>
2023-02-11 11:36:35 -0800

    improved Positions class

--- src/main/java/frc/robot/utilities/Positions.java
+++ src/main/java/frc/robot/utilities/Positions.java
@@ -1,7 +1,8 @@
 package frc.robot.utilities;
 
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 
 public class Positions {
     public static class Pose3d {
@@ -26,6 +27,18 @@ public class Positions {
             return new Pose3d(pose.plus(otherToRobot));
         }
 
+        public static Pose3d fromRobotSpace(Translation3d pose) {
+            return fromRobotSpace(new edu.wpi.first.math.geometry.Pose3d(pose, new Rotation3d()));
+        }
+
+        public static Pose3d fromFieldSpace(Translation3d pose, edu.wpi.first.math.geometry.Pose3d robotPose) {
+            return fromFieldSpace(new edu.wpi.first.math.geometry.Pose3d(pose, new Rotation3d()), robotPose);
+        }
+
+        public static Pose3d fromOtherSpace(Translation3d pose, Transform3d robotToOther) {
+            return fromOtherSpace(new edu.wpi.first.math.geometry.Pose3d(pose, new Rotation3d()), robotToOther);
+        }
+
         public edu.wpi.first.math.geometry.Pose3d inRobotSpace() {
             return pose;
         }
@@ -37,6 +50,18 @@ public class Positions {
 
         public edu.wpi.first.math.geometry.Pose3d inOtherSpace(Transform3d robotToOther) {
             return pose.transformBy(robotToOther);
+        }
+
+        public Translation3d inRobotSpaceTranslation3d() {
+            return pose.getTranslation();
+        }
+
+        public Translation3d inFieldSpaceTranslation3d(edu.wpi.first.math.geometry.Pose3d robotPose) {
+            return inFieldSpace(robotPose).getTranslation();
+        }
+
+        public Translation3d inOtherSpaceTranslation3d(Transform3d robotToOther) {
+            return inOtherSpace(robotToOther).getTranslation();
         }
     }
 }
