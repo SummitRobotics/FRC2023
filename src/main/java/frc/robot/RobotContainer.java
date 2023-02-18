14c2eb3e48c1db1a1f221f3abb6f283daa048d99
Owen McCormick <oweninoregon@gmail.com>
2023-02-18 13:00:32 -0800

    Fixed errors in RobotContainer from merge

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,12 +5,10 @@
 package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.FullManualArm;
 import frc.robot.commands.drivetrain.ArcadeDrive;
 import frc.robot.oi.drivers.ControllerDriver;
@@ -34,7 +32,7 @@ public class RobotContainer {
   private Drivetrain drivetrain;
   private Arm arm;
   // private Intake intake;
-  private AHRS gyro;
+  private AHRS navx;
 
   public RobotContainer() {
     scheduler = CommandScheduler.getInstance();
@@ -60,16 +58,14 @@ public class RobotContainer {
       driverXBox.dPadAny
     );
 
+    setDefaultCommands();
+
     // Configure the bindings
     configureBindings();
 
     // Init Logging and Telemetry
     initLogging();
     initTelemetry();
-  }
-
-  private void initTelemetry() {
-    SmartDashboard.putData("Drivetrain", drivetrain);
   }
 
   private void configureBindings() {
