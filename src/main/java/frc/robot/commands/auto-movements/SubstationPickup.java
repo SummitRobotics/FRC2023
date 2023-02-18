c6212af6b22cc8c255ffda520a61edd83072d390
Eric Verheyden <erve2005@gmail.com>
2023-02-18 12:34:22 -0800

    made better

--- src/main/java/frc/robot/commands/auto-movements/SubstationPickup.java
+++ src/main/java/frc/robot/commands/auto-movements/SubstationPickup.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -60,13 +58,7 @@ public class SubstationPickup extends SequentialCommandGroup {
             new MoveArm(
                 arm,
                 Positions.Pose3d.fromFieldSpace(
-                    gamePiece,
-                    new Pose3d(
-                        drivetrain.getPose().getX(),
-                        drivetrain.getPose().getY(),
-                        0,
-                        new Rotation3d(0, 0, drivetrain.getPose().getRotation().getRadians())
-                    )
+                    gamePiece
                 ),
                 0.0,
                 0.0
