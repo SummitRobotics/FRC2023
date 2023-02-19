9ab380e4c04c64a131b0a1136465dbfcd04a4941
Owen McCormick <41201059+owen-mccormick@users.noreply.github.com>
2023-02-18 16:00:55 -0800

    Added TimedMoveMotor (#31)

---
+++ src/main/java/frc/robot/commands/TimedMoveMotor.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleConsumer;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TimedMoveMotor extends CommandBase {
+
+    DoubleConsumer powerSetter;
+    double power;
+    double seconds;
+    Timer timer;
+    
+    public TimedMoveMotor(DoubleConsumer powerSetter, double power, double seconds) {
+        this.powerSetter = powerSetter;
+        this.power = power;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void initialize() {
+        timer.start();
+    }
+    
+    @Override
+    public void execute() {
+        powerSetter.accept(power);
+    }
+
+    @Override
+    public void end(final boolean interrupted) {
+        timer.stop();
+        powerSetter.accept(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(seconds);
+    }
+}
