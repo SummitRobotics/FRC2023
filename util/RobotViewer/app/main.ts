8638a5eae86fe86e6badec66102a5805b1c33fdc
John <farkyoulater@yahoo.com>
2023-02-25 11:13:15 -0800

    Switch to ntcore-ts-client

--- util/RobotViewer/app/main.ts
+++ util/RobotViewer/app/main.ts
@@ -1,30 +1,8 @@
 import { app, BrowserWindow, ipcMain, ipcRenderer } from 'electron'
-import { Client } from 'wpilib-nt-client';
 import path from 'path';
+import { NetworkTables, NetworkTablesTypeInfos } from 'ntcore-ts-client';
 
-/**
- * Constructs a networktable client and connects to remote server.
- *
- * @param address The address of the networktable server.
- * @param connectDelay Delay before attempting to reconnected (ms).
- * @returns networktable client
- */
-const createNtClient = (address: string, connectDelay: number = 1000): Client => {
-    const client = new Client();
-    client.setReconnectDelay(connectDelay);
-    let hasConnected = false; // Client doesn't perform a reconnect until connected
-    const connectHandler = (connected: boolean, err: Error) => {
-        hasConnected = hasConnected || connected;
-        console.log({ connected, err });
-        if (!hasConnected) {
-            setTimeout(() => {
-                client.start(connectHandler, address);
-            }, connectDelay);
-        }
-    };
-    client.start(connectHandler, address);
-    return client;
-}
+const DEBUG = false;
 
 const createWindow = (): BrowserWindow => {
     const win = new BrowserWindow({
@@ -39,27 +17,55 @@ const createWindow = (): BrowserWindow => {
 }
 
 async function main() {
-    // const ntClient = createNtClient('127.0.0.1');
-    const ntClient = createNtClient('10.54.68.2');
+    const ntClient = NetworkTables.getInstanceByURI('127.0.0.1');
+    // const ntClient = NetworkTables.getInstanceByURI('10.54.68.2');
 
     await app.whenReady();
     const win = createWindow();
 
+    const subscribe = () => {
+        // TODO: don't brute force this
+        const names = [
+            '/SmartDashboard/Arm/turretAngle',
+            '/SmartDashboard/Arm/firstJointAngle',
+            '/SmartDashboard/Arm/secondJointAngle',
+            '/SmartDashboard/Arm/thirdJointAngle',
+            '/SmartDashboard/Arm/wristAngle',
+            '/SmartDashboard/Arm/grabberClamp',
+        ];
+
+        names.forEach((name: string) => {
+            // NOTE: default to subscribe as kDouble type; values of all types still come through
+            const topic = ntClient.createTopic<number>(name, NetworkTablesTypeInfos.kDouble);
+            topic.subscribe((val: number) => {
+                win.webContents.send('update', name, val);
+                if (DEBUG) {
+                    console.log(`> ${name}: ${val}`);
+                }
+            }, true);
+        });
+    };
+
     // Bind IPC methods
     ipcMain.handle('set', (_, name: string, val: any) => {
-        console.log(`set: ${name} - ${val}`);
-        ntClient.Assign(val, name);
-    });
-    ntClient.addListener((key: string, value: any, valueType: string, type: string, id: number, flags: number) => {
-        win.webContents.send('update', key, value, valueType, type, id, flags);
-        //console.log(`update: ${key} - ${value} - ${valueType} - ${type} - ${id} - ${flags}`);
+        // TODO as needed (see: https://github.com/Chris2fourlaw/ntcore-ts-client/blob/main/README.md)
+        //  ntClient.createTopic
+        //  ntClient.publish
+        //  ntClient.setValue
+        // Wait until renderer is ready to subscribe and receive initial update
+        if (name === 'rendererReady') {
+            subscribe();
+        }
+        if (DEBUG) {
+            console.log(`< ${name}: ${val}`);
+        }
     });
 
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') {
             app.quit();
         }
-    })
+    });
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
